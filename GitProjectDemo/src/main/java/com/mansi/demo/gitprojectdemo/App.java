0891fdb7fa6913e09b22bbd9380a713d31a37fad
MansiBorwanker <mansiborwanker.mb@gmail.com>
2019-05-22 11:48:53 +0530

    project commit

---
+++ GitProjectDemo/src/main/java/com/mansi/demo/gitprojectdemo/App.java
@@ -0,0 +1,13 @@
+package com.mansi.demo.gitprojectdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
